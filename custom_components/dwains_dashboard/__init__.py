4e2ce9e9c3431f9baf034525d6e981a56b12b1e2
xBourner <64064679+xBourner@users.noreply.github.com>
2024-04-18 07:16:49 +0200

    Add files via upload

--- custom_components/dwains_dashboard/__init__.py
+++ custom_components/dwains_dashboard/__init__.py
@@ -448,6 +448,7 @@ async def ws_handle_edit_area_bool_value(
         vol.Optional("disableWelcomeMessage"): bool,
         vol.Optional("v2Mode"): bool,
         vol.Optional("disableSensorGraph"): bool,
+        vol.Optional("invertCover"): bool,
         vol.Optional("weatherEntity"): str,
         vol.Optional("alarmEntity"): str,
 
@@ -471,6 +472,7 @@ async def ws_handle_edit_homepage_header(
         "disable_welcome_message": msg["disableWelcomeMessage"],
         "v2_mode": msg["v2Mode"],
         "disable_sensor_graph": msg["disableSensorGraph"],
+        "invert_cover": msg["invertCover"],
         "weather_entity": msg["weatherEntity"],
         "alarm_entity": msg["alarmEntity"],
     })
